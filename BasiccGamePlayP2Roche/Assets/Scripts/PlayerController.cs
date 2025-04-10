d2c5d5525f4616150c8db0b512bbb0d49b4911e8
JunkyW0nky <723469@student.dusd.net>
2025-04-10 09:54:37 -0700

    rehh
    raaahhh

--- BasiccGamePlayP2Roche/Assets/Scripts/PlayerController.cs
+++ BasiccGamePlayP2Roche/Assets/Scripts/PlayerController.cs
@@ -18,11 +18,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if  (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Launch a projectile froom the player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
         // Keep the player in bounds
         if (transform.position.x < -xRange)
         {
@@ -31,8 +26,14 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile froom the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
